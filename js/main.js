e17c7db8786da2a5104df87bf0308609c4943c78
christophe <bazin.christophe@gmail.com>
2017-02-07 10:18:42 +0100

    Ajout du sorting

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var key = "4ed75158af45aeeb2df0525d0af7e52c";
 var token = "159afbf454811016be493bce6978b565c52f0d2a6669a0d969e3db9d533ad060";
 var board = "9TgrKdIZ";
 
-var trelloUrl = "https://api.trello.com/1/boards/"+ board +"/cards?key="+ key +"&token="+ token +"&fields=url,name,shortLink,idMembersVoted,labels";
+var trelloUrl = "https://api.trello.com/1/boards/"+ board +"/cards?key="+ key +"&token="+ token +"&fields=url,name,shortLink,idMembersVoted,labels,dateLastActivity";
 console.log("Sortie de l'API Trello", trelloUrl);
 
 $.ajax({
@@ -34,7 +34,14 @@ function drawRow(rowData) {
 	var row = $("<tr class='Table-row'/>")
 	$("#trelloDatas").append(row);
 	row.append($("<td class='Table-cell u-tal'>" + rowData.name + "</td>"));
+	row.append($("<td class='Table-cell u-tal'>" + rowData.dateLastActivity + "</td>"));
 	row.append($("<td class='Table-cell'><a href="+ rowData.url +" target=_blank>" + rowData.shortLink + "</a></td>"));
 	row.append($("<td class='Table-cell u-tal'>" + Object.values(labelsNames) +"</td>"));
 	row.append($("<td class='Table-cell'>" + Object.keys(rowData.idMembersVoted).length +"</td>"));
+
+	$(function(){
+		$("#trelloDatas").tablesorter();
+	});
 }
+
+
