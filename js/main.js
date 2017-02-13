6bbe4a143f435fa384ca2ab6aedb51a3258be146
christophe <christophe@wisembly.com>
2017-02-13 16:06:50 +0100

    Improve button & search bar

--- js/main.js
+++ js/main.js
@@ -34,8 +34,6 @@ $("#connectLink").click(function() {
 
 $("#disconnect").click(logout);
 
-// 
-
 var key = "4ed75158af45aeeb2df0525d0af7e52c";
 var token = "159afbf454811016be493bce6978b565c52f0d2a6669a0d969e3db9d533ad060";
 var board = "9TgrKdIZ";
