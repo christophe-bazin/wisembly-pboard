f421827a807e8ef11ae14fe4b9c43c53e580e9e5
christophe <christophe@wisembly.com>
2017-02-15 15:45:35 +0100

    Improve JS files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+// TableSorter config
 $("#trelloDatas").tablesorter( {sortList: [[4,1], [5,1]]} ); 
 
+// Trello authorize (with client.js)
 var onAuthorize = function() {
 	updateLoggedIn();
 	$("#output").empty();
@@ -8,23 +10,19 @@ var onAuthorize = function() {
 		window.idMemberVoted = member.id;
 	});
 };
-
 var updateLoggedIn = function() {
 	var isLoggedIn = Trello.authorized();
 	$("#loggedout").toggle(!isLoggedIn);
 	$("#loggedin").toggle(isLoggedIn);
 };
-
 var logout = function() {
 	Trello.deauthorize();
 	updateLoggedIn();
 };
-
 Trello.authorize({
 	interactive: false,
 	success: onAuthorize
 });
-
 $("#connectLink").click(function() {
 	Trello.authorize({
 		type: "popup",
@@ -32,78 +30,72 @@ $("#connectLink").click(function() {
 		scope: { read: true, write: true },
 	});
 });
-
 $("#disconnect").click(logout);
 
-var getCards = function() {
-
-};
-
-var key = "4ed75158af45aeeb2df0525d0af7e52c";
-var token = "159afbf454811016be493bce6978b565c52f0d2a6669a0d969e3db9d533ad060";
-var board = "9TgrKdIZ";
-
+// Vote function
+$.getScript('js/trello.js', function() {
 var trelloUrl = "https://api.trello.com/1/boards/"+ board +"/cards?key="+ key +"&token="+ token +"&fields=url,name,shortLink,idMembersVoted,labels,dateLastActivity";
 
-$.ajax({
-	url: trelloUrl,
-	type: "get",
-	dataType: "json",
-	data: {
-		json: JSON.stringify(trelloUrl),
-		delay: 3
-	},
-	success: function(data, textStatus, jqXHR) {
-		getTrelloData(data);
+	$.ajax({
+		url: trelloUrl,
+		type: "get",
+		dataType: "json",
+		data: {
+			json: JSON.stringify(trelloUrl),
+			delay: 3
+		},
+		success: function(data, textStatus, jqXHR) {
+			getTrelloData(data);
+		}
+	});
+	function getTrelloData(data) {
+		for (var i = 0; i < data.length; i++) {
+			displayTrelloDatas(data[i]);
+		}
+	}
+	function displayTrelloDatas(displayTrelloData) {
+
+		$a = displayTrelloData.idMembersVoted // Liste des votants sur trello 
+		$b = idMemberVoted // Mon id
+
+		if ($a.indexOf($b) !== -1) {
+			$('.' + displayTrelloData.id + ' .vote').attr('class', 'Btn Btn--validate voted')
+		}
+
+		$('.' + displayTrelloData.id + ' .vote').click(function() {
+			$.ajax({
+				type: 'POST',
+				data: "key="+ key +"&token="+ (Trello.token() || token) +"&value="+ idMemberVoted +"",
+				success: function(success) { 
+					$('.' + displayTrelloData.id + ' .vote').attr('class', 'Btn Btn--validate voted').find('.count').html(parseInt($('.' + displayTrelloData.id + ' .count').html())+1)	
+					$(document).ajaxStop(function() { (location).reload(true); });
+					console.log("success") 
+				},
+				error: function(error){ console.log("error") },
+				url: "https://www.trello.com/1/cards/"+ displayTrelloData.id +"/membersVoted?",
+				cache:false
+			});
+		});
+
+		$('.' + displayTrelloData.id + ' .voted').click(function() {
+			$.ajax({
+				type: 'DELETE',
+				data: "key="+ key +"&token="+ (Trello.token() || token) +"&value="+ idMemberVoted +"",
+				success: function(success) { 
+					$('.' + displayTrelloData.id + ' .voted').attr('class', 'Btn vote').find('.count').html(parseInt($('.' + displayTrelloData.id + ' .count').html())-1)	
+					$(document).ajaxStop(function() { location.reload(true); });
+					console.log("success") 
+				},
+				error: function(error){ console.log("error") },
+				url: "https://www.trello.com/1/cards/"+ displayTrelloData.id +"/membersVoted/" + idMemberVoted +"",
+				cache:false
+			});
+		});
 	}
 });
 
-function getTrelloData(data) {
-	for (var i = 0; i < data.length; i++) {
-		displayTrelloDatas(data[i]);
-	}
-}
 
-function displayTrelloDatas(displayTrelloData) {
-
-	$a = displayTrelloData.idMembersVoted // Liste des votants sur trello 
-	$b = idMemberVoted // Mon id
-
-	if ($a.indexOf($b) !== -1) {
-		$('.' + displayTrelloData.id + ' .vote').attr('class', 'Btn Btn--validate voted')
-	}
-
-	$('.' + displayTrelloData.id + ' .vote').click(function() {
-		$.ajax({
-			type: 'POST',
-			data: "key="+ key +"&token="+ (Trello.token() || token) +"&value="+ idMemberVoted +"",
-			success: function(success) { 
-				$('.' + displayTrelloData.id + ' .vote').attr('class', 'Btn Btn--validate voted').find('.count').html(parseInt($('.' + displayTrelloData.id + ' .count').html())+1)	
-				$(document).ajaxStop(function() { (location).reload(true); });
-				console.log("success") 
-			},
-			error: function(error){ console.log("error") },
-			url: "https://www.trello.com/1/cards/"+ displayTrelloData.id +"/membersVoted?",
-			cache:false
-		});
-	});
-
-	$('.' + displayTrelloData.id + ' .voted').click(function() {
-		$.ajax({
-			type: 'DELETE',
-			data: "key="+ key +"&token="+ (Trello.token() || token) +"&value="+ idMemberVoted +"",
-			success: function(success) { 
-				$('.' + displayTrelloData.id + ' .voted').attr('class', 'Btn vote').find('.count').html(parseInt($('.' + displayTrelloData.id + ' .count').html())-1)	
-				$(document).ajaxStop(function() { location.reload(true); });
-				console.log("success") 
-			},
-			error: function(error){ console.log("error") },
-			url: "https://www.trello.com/1/cards/"+ displayTrelloData.id +"/membersVoted/" + idMemberVoted +"",
-			cache:false
-		});
-	});
-}
-
+// Search function
 $('#search_field').on('keyup', function() {
 	var value = $(this).val();
 	var patt = new RegExp(value, "i");
